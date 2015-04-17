d4b7f8dd02bfdd21610fb7ebea19edcb6ebfcc29
Volodymyr Kuzmenko <VolodymyrKuzmenko@users.noreply.github.com>
2015-04-17 20:21:35 +0300

    Update CalculationWorker.java
    fixed head border

--- src/pro2_lab6/CalculationWorker.java
+++ src/pro2_lab6/CalculationWorker.java
@@ -7,9 +7,9 @@ package pro2_lab6;
  *                                                                           *
  * Task: MA = (B*C)*MO + α*(MT*MR)                                           *
  *                                                                           *
- * @file CalculationWorker.java 										     *
- * @author Kuzmenko Volodymyr					                             *
- * @group IO-21								                                 *
+ * @file CalculationWorker.java 					     *
+ * @author Kuzmenko Volodymyr					             *
+ * @group IO-21								     *
  * @date 17.04.2015                                                          *
  *                                                                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
