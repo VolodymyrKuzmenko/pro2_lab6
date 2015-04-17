bbd148775c681f151f3374a30b3a60b9ce310bbd
Volodymyr Kuzmenko <VolodymyrKuzmenko@users.noreply.github.com>
2015-04-17 20:25:17 +0300

    Update SynchronizationMonitor.java
    fixed head borer

--- src/pro2_lab6/SynchronizationMonitor.java
+++ src/pro2_lab6/SynchronizationMonitor.java
@@ -6,9 +6,9 @@ package pro2_lab6;
  *                                                                           *
  * Task: MA = (B*C)*MO + α*(MT*MR)                                           *
  *                                                                           *
- * @file SynchronizationMonitor.java 				     				     *
- * @author Kuzmenko Volodymyr					                             *
- * @group IO-21								                                 *
+ * @file SynchronizationMonitor.java 				     	     *
+ * @author Kuzmenko Volodymyr					             *
+ * @group IO-21								     *
  * @date 17.04.2015                                                          *
  *                                                                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
