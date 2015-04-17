eb2630f30987c538fb91d2a0382363043d7d35cb
Volodymyr <kuzmenko.volodimir@gmail.com>
2015-04-17 17:36:50 +0300

    start

---
+++ src/pro2_lab6/Executor.java
@@ -0,0 +1,13 @@
+package pro2_lab6;
+
+public class Executor {
+	private static int N = 24;
+	private static int P = 6;
+	private static int H = N / P;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
