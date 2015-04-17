43b191897b02e8077efddc0f1cdce0b1fc4de421
Volodymyr <kuzmenko.volodimir@gmail.com>
2015-04-17 18:26:55 +0300

    add resourse monitor

--- src/pro2_lab6/GeneralResourseMonitor.java
+++ src/pro2_lab6/GeneralResourseMonitor.java
@@ -1,5 +1,31 @@
 package pro2_lab6;
 
 public class GeneralResourseMonitor {
-
+	private Matrix MR;
+	private int v = 0;
+	private int alfa;
+	
+	public synchronized void setAlfa(int value){
+		this.alfa = value;
+	}
+	
+	public synchronized void addV(int value){
+		this.v += value;
+	}
+	
+	public synchronized void setMR(Matrix value){
+		this.MR = value;
+	}
+	
+	public synchronized int copyAlfa(){
+		return this.alfa;
+	}
+	
+	public synchronized int copyV(){
+		return this.v;
+	}
+	
+	public synchronized Matrix copyMR(){
+		return this.MR;
+	}
 }
